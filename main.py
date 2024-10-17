78e7ac025a60047d85b87e4f8a6c41a16fa3cfc1
slantsh <joeltelesaj@gmail.com>
2024-10-17 17:41:53 +0530

    finish 2

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import pygame
 import time
 
-
 pygame.init()
-timer=pygame.time.Clock()
-screen=pygame.display.set_mode([1280,720])
+timer = pygame.time.Clock()
+screen = pygame.display.set_mode([1280, 720])
 font = pygame.font.Font('assets/NotoSans-VariableFont_wdth,wght.ttf', 48)
-color_inactive = pygame.Color('lightskyblue3')
-color_active = pygame.Color('dodgerblue2')
-color = color_inactive
-current_bpm=60
-CHORDS= {
+current_bpm = 60
+line_pos = -160
+CHORDS = {
     "C": ["C", "E", "G"],
     "Cm": ["C", "D#", "G"],
     "D": ["D", "F#", "A"],
@@ -27,119 +24,112 @@ CHORDS= {
     "Bm": ["B", "D", "F#"]
 }
 
-CHORDLIST=list(CHORDS.keys())
+CHORDLIST = list(CHORDS.keys())
 CHORDLIST.sort()
-
-
+color_inactive = pygame.Color('lightskyblue3')
+color_active = pygame.Color('dodgerblue2')
 
 
 def player(c):
-        s=[]
-        pygame.mixer.stop()
-        for i in c:
-            s.append(pygame.mixer.Sound('assets/notes/'+i+'.wav'))
-        for x,j in enumerate(s):
-            pygame.mixer.Channel(x).play(j)
+    s = []
+    pygame.mixer.stop()
+    for i in c:
+        s.append(pygame.mixer.Sound('assets/notes/' + i + '.wav'))
+    for x, j in enumerate(s):
+        pygame.mixer.Channel(x).play(j)
 
 
-plus=pygame.transform.scale(pygame.image.load('assets/plus.png'),(80,80))
-minus=pygame.transform.scale(pygame.image.load('assets/minus.png'),(80,80))
-plusrect=pygame.Rect([1110, 460, 80, 80])
-minusrect=pygame.Rect([1110, 610, 80, 80])
+progression = []
+playing = False
+last_play_time = time.time()
+play_index = 0
 
-progression=[]
-def tracker():
-    if len(progression)>4:
-        progression.pop()
-
-playing=False
-last_play_time=time.time()
-play_index=0
-
-rythm1='0 000 1 111 2 222 3 333'
-
-running=True
+running = True
 while running:
     timer.tick(60)
     screen.fill('black')
-    c=0
-    buttonlist=[]
-    for y in [450,600]:
+    c = 0
+    buttonlist = []
+
+    # Drawing chord buttons
+    for y in [450, 600]:
         for i in range(7):
-            if CHORDS[CHORDLIST[c]] in progression:
-                rectcolor=color_active
-            else:
-                rectcolor=color_inactive
-            pygame.draw.rect(screen,rectcolor,[20+(i*150),y,100,100],border_radius=12) 
-            buttonlist.append(pygame.Rect([20+(i*150),y,100,100]) )       
-            clabel=font.render(CHORDLIST[c],True,'black')
-            c+=1
-            screen.blit(clabel,[30+(i*150),y])
-    pygame.draw.rect(screen,'darkgray',[20,20,1240,410],border_radius=12)
+            rectcolor = color_active if CHORDS[CHORDLIST[c]] in progression else color_inactive
+            pygame.draw.rect(screen, rectcolor, [20 + (i * 150), y, 100, 100], border_radius=12)
+            buttonlist.append(pygame.Rect([20 + (i * 150), y, 100, 100]))
+            clabel = font.render(CHORDLIST[c], True, 'black')
+            c += 1
+            screen.blit(clabel, [30 + (i * 150), y])
 
-    playbutton=pygame.Rect([1140,40,100,100])
-    pygame.draw.rect(screen,'#81FE9A',playbutton,border_radius=12)
+    pygame.draw.rect(screen, color_inactive, [20, 20, 1240, 410], border_radius=12)
 
-    stopbutton=pygame.Rect([1140,160,100,100])
-    pygame.draw.rect(screen,'#FE8181',stopbutton,border_radius=12)
+    # Play and Stop buttons
+    playbutton_rect = pygame.Rect([1100, 40, 100, 100])
+    pygame.draw.rect(screen, '#81FE9A', playbutton_rect, border_radius=12)
 
+    stopbutton_rect = pygame.Rect([1100, 160, 100, 100])
+    pygame.draw.rect(screen, '#FE8181', stopbutton_rect, border_radius=12)
 
-    bpm_box = pygame.Rect(1100, 450, 100, 250)
-    bpm_label=font.render(str(current_bpm),True,'black')
-    pygame.draw.rect(screen,color_inactive,bpm_box,border_radius=12)
-    screen.blit(bpm_label,(1115, 540, 100, 250))
-    screen.blit(plus,plusrect)
-    screen.blit(minus,minusrect)
+    # BPM display and adjustment
+    bpm_rect = pygame.Rect(1100, 450, 100, 250)
+    bpm_label = font.render(str(current_bpm), True, 'black')
+    pygame.draw.rect(screen, color_inactive, bpm_rect, border_radius=12)
+    screen.blit(bpm_label, (1115, 540, 100, 250))
 
+    plus = pygame.transform.scale(pygame.image.load('assets/plus.png'), (80, 80))
+    minus = pygame.transform.scale(pygame.image.load('assets/minus.png'), (80, 80))
+    plus_rect = pygame.Rect([1110, 460, 80, 80])
+    minus_rect = pygame.Rect([1110, 610, 80, 80])
+    screen.blit(plus, plus_rect)
+    screen.blit(minus, minus_rect)
 
+    # Progression line
+    for i in [120, 400, 680, 960]:
+        pygame.draw.rect(screen, "black", [i, 20, 10, 410])
+    pygame.draw.rect(screen, "white", [line_pos, 20, 10, 410])
 
+    # Event handling
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            pos=pygame.mouse.get_pos()
-            for index,b in enumerate(buttonlist):
+        pos = pygame.mouse.get_pos()
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for index, b in enumerate(buttonlist):
                 if b.collidepoint(pos):
-                    chord_to_play=CHORDS[CHORDLIST[index]]
-                    progression.append(chord_to_play)
+                    chord_to_play = CHORDS[CHORDLIST[index]]
+                    if chord_to_play not in progression:
+                        progression.append(chord_to_play)
+                    else:
+                        progression.remove(chord_to_play)
                     if len(progression) > 4:
                         del progression[0]
-            if playbutton.collidepoint(pos):
-                playing=True
+
         if pygame.mouse.get_pressed()[0]:
-            if plusrect.collidepoint(pos):
-                current_bpm+=1
-            if minusrect.collidepoint(pos):
-                current_bpm-=1
+            if playbutton_rect.collidepoint(pos):
+                playing = True
+            if plus_rect.collidepoint(pos):
+                current_bpm = min(current_bpm + 1, 300)
+            if minus_rect.collidepoint(pos):
+                current_bpm = max(current_bpm - 1, 30)
 
-    
+    # Handle playback
     if playing:
-        current_time = time.time()          
-        if current_time - last_play_time >= 60/current_bpm:  
+        current_time = time.time()
+        time_between = current_time - last_play_time
+
+        if time_between >= 60 / current_bpm:
             if play_index < len(progression):
-                player(progression[play_index])  
-                play_index += 1  
-                last_play_time = current_time 
+                player(progression[play_index])
+                play_index += 1
+                last_play_time = current_time
+                line_pos += 280
             else:
-                play_index = 0  
-        if stopbutton.collidepoint(pygame.mouse.get_pos()):
-            if event.type==pygame.MOUSEBUTTONDOWN:
-                playing=False
-        
-        
+                play_index = 0
+                line_pos = -160
 
-
+        if stopbutton_rect.collidepoint(pygame.mouse.get_pos()):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                playing = False
 
     pygame.display.flip()
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
