698438cb81d50a32e1b478c01788c6529dd252db
slantsh <joeltelesaj@gmail.com>
2024-10-22 19:03:25 +0530

    1

--- getchords.py
+++ getchords.py
@@ -22,23 +22,17 @@ def fetch_chords():
         "Bm": ["B", "D", "F#"]
     }
 
-    # Create Database if it doesn't exist
-    cursor.execute('CREATE DATABASE concorde IF NOT EXISTS')
-
-    # Switch to the 'concorde' database
+    cursor.execute('CREATE DATABASE IF NOT EXISTS concorde')
     cursor.execute('USE concorde')
-
-    # Create the 'chords' table if it doesn't exist
     cursor.execute('CREATE TABLE IF NOT EXISTS chords (chord_name VARCHAR(10) PRIMARY KEY, note1 VARCHAR(2), note2 VARCHAR(2), note3 VARCHAR(2))')
     connection.commit()
-    # Insert chords into the table if the table was just created
     cursor.execute("SELECT COUNT(*) FROM chords")
     if cursor.fetchone()[0] == 0:  # If no data exists in the table
         for chord, notes in CHORDS.items():
             cursor.execute("INSERT INTO chords (chord_name, note1, note2, note3) VALUES ('{}', '{}', '{}', '{}')".format(chord, notes[0], notes[1], notes[2]))
         connection.commit()
 
-    # Query to select all chords
+
     cursor.execute("SELECT * FROM chords")
     
     # Fetch all results
